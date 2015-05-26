5eb5b6d4c9dcaf1ddaa9a9e2ac1e04e33391d422
Brandon Price <brandonadamprice@gmail.com>
2015-05-26 14:01:40 -0400

    Create DisableAppNap.sh
    Disables AppNap for both chrome apps and birdbrain server

---
+++ Chrome Apps/DisableAppNap.sh
@@ -0,0 +1,5 @@
+defaults write com.google.Chrome.app.Default-gkedchhhodbbiojogfhkfieobopcmjnj NSAppSleepDisabled -bool YES
+
+defaults write com.google.Chrome.app.Default-agekgdamojgfpbfknfebdopkokldcjko NSAppSleepDisabled -bool YES
+
+defaults write com.birdbraintechnologies.robotserver NSAppSleepDisabled -bool YES
